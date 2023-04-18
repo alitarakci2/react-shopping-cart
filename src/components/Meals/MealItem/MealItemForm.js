dee74eb6a0175ccea74cfb1455ae5ed30ab6dba7
Ali <101870797+alitarakci2@users.noreply.github.com>
2023-04-18 15:20:14 +0300

    adding context and managing Cart and some animations

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,32 @@
+import { useRef, useState } from 'react';
 import classes from './MealItemForm.module.css';
 import  Input from '../../UI/Input';
 
 const MealItemForm = props => {
+    const [amountISValid, setAmountIsValid] = useState(true);
+    const amountInputRef = useRef();
+    const submitHandler = event => {
+        event.preventDefault();
 
-    return <form className={classes.form}>
-        <Input label="Amount" input={{
+        const enteredAmount = amountInputRef.current.value;
+        const enteredAmountNumber = +enteredAmount;
+
+        if(enteredAmount.trim().length === 0 || 
+        enteredAmountNumber < 1 || 
+        enteredAmountNumber > 5) {
+            setAmountIsValid(false);
+            return;
+        }
+
+        props.onAddToCart(enteredAmountNumber);
+
+    };
+
+    return <form className={classes.form} onSubmit={submitHandler}>
+        <Input 
+        ref={amountInputRef}
+        label="Amount"
+         input={{           
             id: 'amount_' + props.id,
             type: 'number',
             min: '1',
@@ -13,6 +35,7 @@ const MealItemForm = props => {
             defaultValue: '1'
         }}/>
         <button>+ Add</button>
+        {!amountISValid && <p>Plase enter a valid amount (1-5)</p>}
     </form>
 };
 
