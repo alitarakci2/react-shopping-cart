2473e17b9861537cdbb65bb591c45f5f5131cb3c
Ali <101870797+alitarakci2@users.noreply.github.com>
2023-04-18 13:14:50 +0300

    Setting up cart context and Cart components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
 
 
 function App() {
+
+  const [cartIsShown, setCardIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCardIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCardIsShown(false);
+  };
   return (
     <Fragment>
-     <Header />
+      { cartIsShown && <Cart onClose={hideCartHandler}/>}
+     <Header onShowCart={showCartHandler}/>
      <main>
       <Meals />
      </main>
---
+++ src/components/UI/Modal.js
@@ -0,0 +1,35 @@
+
+import { Fragment } from 'react';
+import { createPortal } from "react-dom";
+import classes from './Modal.module.css'
+
+
+
+const BackDrop = (props) => {
+    return (<div className={classes.backdrop} onClick={props.onClose}/>);
+
+
+};
+
+const ModalOverlay = (props) => {
+    return (
+    <div className={classes.modal}>
+        <div >{props.children}</div>
+
+    </div>
+    );
+
+};
+
+const portalElement = document.getElementById('overlays');
+
+const Modal = (props) => {
+        return (
+        <Fragment>
+            {createPortal(<BackDrop onClose={props.onClose}/>, portalElement)}
+            {createPortal(<ModalOverlay>{props.children}</ModalOverlay>, portalElement)}
+        </Fragment>
+         );
+};
+
+export default Modal;
